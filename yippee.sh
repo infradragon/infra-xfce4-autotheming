278afe25d8a212be45b72ad26ae0a31f3ff37c84
infradragon <72665124+infradragon@users.noreply.github.com>
2025-01-15 06:35:53 -0600

    make it actualyl work

--- yippee.sh
+++ yippee.sh
@@ -4,7 +4,8 @@ cd /var/tmp
 git clone https://github.com/vinceliuice/Orchis-theme
 chmod +x ./Orchis-theme/install.sh
 ./Orchis-theme/install.sh -i debian -s compact -c dark --tweaks black -t red -n Orchis-Root
-sudo cp -fv ~/.themes/Orchis-Root-Red-Dark-Compact* /root/.themes/
+sudo mkdir /root/.themes/
+sudo cp -rfv ~/.themes/Orchis-Root-Red-Dark-Compact* /root/.themes/
 ./Orchis-theme/install.sh -i debian -s compact -c dark --tweaks dracula -l
 rm -rfv /var/tmp/Orchis-theme
 
